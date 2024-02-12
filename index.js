b4f0fcf8aaebcb7d218ab8ffb63a520eb4f5a0a9
Göran Olson <goran@famolson.se>
2024-02-12 13:39:17 +0100

    created random product on start page

--- index.js
+++ index.js
@@ -3,40 +3,91 @@ const maxPage = 10;
 
 const mainProduct = document.getElementById('main-product');
 
-
+const display = document.getElementById('display');
 
 
 
 const targetList = getTarget();
  console.log('targetList:', targetList);
- if (targetList) {
-    console.log('got targetList');
- }
+if (targetList) {
+console.log('got targetList');
+}
 
 // 0: prod | name | page
 // 1: value
+const target = (targetList) ? targetList[0] : '';
+ console.log(`target: ${target}`);
+switch (target) {
+    case 'prod':
+        displayProduct(targetList[1]);
+        break;
+
+    default:
+        // console.log('No target...');
+        displayRandom();
+        break;
+}
+
+async function displayRandom() {
+    // Random
+    console.log('--> displayDefault()');
+
+    const randomURL = "https://api.punkapi.com/v2/beers/random";
+
+    try {
+        const response = await fetch(randomURL);
+        const data = await response.json();
+        // console.log('data:', data);
+        const product = data[0];
+         console.log('product:', product);
+
+        const imagePath = (product.image_url) ? product.image_url : '';
+         
+        const article = `
+            <article id="main-product" class="card">
+                <img src="${imagePath}" alt="${product.name}">
+                <div class="card-content">
+                    <h4 title="${product.name}">${product.name}</h4> 
+                    <p>${product.tagline}</p> 
+                    <p>${product.abv} alcohol</p> 
+                    <p>
+                        <a href="index.html?prod=${product.id}">Se More <i class="fas fa-play"></i></a>
+                    </p>
+                </div>
+            </article>`;
+
+        display.innerHTML = article;
+        
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+
+}
 
 
+function displayProduct(prodId) {  // [ 99 ]
+    console.log(`--> displayProduct(${prodId})`);
+}
 
-
-// product:     ?prod=99
-// query:       ?name=test%20
-// paginering:  ?page=2
 
 function getTarget() {  // : array | undefined
-    console.log('--> getTarget()');
+    // console.log('--> getTarget()');
+
+    // product:     ?prod=99
+    // query:       ?name=test%20
+    // paginering:  ?page=2
 
     let arr;
 
     let url = window.location.href;
-    console.log(`url: ${url}`);
+    // console.log(`url: ${url}`);
 
     const pos = url.indexOf('?');
 
     if (pos >= 0) {
         let args = url.substring(pos + 1);  // => prod=99
         arr = args.split('=');
-        console.log(`args: ${args}`);
+        // console.log(`arr: ${arr}`);
     }
 
     return arr;
