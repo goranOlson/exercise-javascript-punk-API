650a2f037e3be93d3b3497141ea0c3e553dc2d79
Göran Olson <goran@famolson.se>
2024-02-15 13:53:34 +0100

    fixed gui for searching

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const MAX_SEARCH = 10;
 
-let actPage = 1;
+let actPage = 0;
+let searchString = '';
 
 const mainProduct = document.getElementById('main-product');
 mainProduct.addEventListener('click', (event) => {
     // console.log('click on article');
     if (event.target.tagName === "A") {
-         console.log('click on seeMore');
+        // console.log('click on seeMore');
         event.preventDefault();
         event.stopPropagation();
 
         // const target = event.target;
         const id = event.target.getAttribute('data-id');
-         console.log('id: ' + id);
+        // console.log('id: ' + id);
         importProduct(id);
     }
 
@@ -26,9 +27,11 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     event.stopPropagation();
-    console.log('submint form');
+    // console.log('submint form');
     importSearch();
 });
+
+const searchResults = document.querySelector('.search-results');
 
 /* ###### Icons/logotype ###### */
 
@@ -53,11 +56,10 @@ iconSearch.addEventListener('click', () => {
 
 const seeMoreButton = document.querySelector('#main-product a');
 if (seeMoreButton) {
-     console.log('adding listener to seeMoreButton');
+    // console.log('adding listener to seeMoreButton');
     seeMoreButton.addEventListener('click', (event) => {
         event.stopPropagation();
-        console.log('seMoreButton()');
-        console.log('');
+        //console.log('seMoreButton()');
     });
 }
 
@@ -67,16 +69,21 @@ randomButton.addEventListener('click', () => {
     importProduct();
 });
 
+const arrowPrevious = document.querySelector('.pager.left');
+arrowPrevious.addEventListener('click', clickPreviousPage);
+
+const arrowNext = document.querySelector('.pager.right');
+arrowNext.addEventListener('click', clickNextPage);
+
 
 
 
 // console.log('script start');
-importProduct();  // Default start with random beer
-
-
+// importProduct();  // Default start with random beer
+displaySearch();
 
 async function importProduct(prodId = 0) {
-     console.log(`--> importProduct(${prodId})`);
+    // console.log(`--> importProduct(${prodId})`);
 
     // Ask for prodId or random
     let url = "https://api.punkapi.com/v2/beers/";
@@ -98,47 +105,8 @@ async function importProduct(prodId = 0) {
     }
 }
 
-async function importSearch() {
-    console.log('--> importSearch()');
-
-    const searchResults = document.querySelector('.search-results');
-    searchResults.innerHTML = '';  // Empty results
-    
-    // actPage = 1; // default
-
-    const value = form.querySelector('input').value;
-     console.log('value: ' + value);
-
-    // https://api.punkapi.com/v2/beers?beer_name=berliner
-    // https://api.punkapi.com/v2/beers?page=1&per_page=10
-
-    let url = "https://api.punkapi.com/v2/beers";
-    url += `?page=1&per_page=11&beer_name=${value}`;
-     console.log('url: ' + url);
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-         console.log('search data: ', data);
-        
-        for (let i = 0; i < data.length; i++) {
-            const item = data[i];
-            const element = `<div class="link" data-id="${item.id}">${item.name}</div>`;
-            searchResults.insertAdjacentHTML('beforeend', element);
-        }
-
-    } catch (error) {
-        
-    }
-
-
-    // < if (page > 1)
-    // > if (more than displayed)
-
-}
-
 function displayProductCard(product) {
-     console.log(`--> displayProduct(product) => ${product.name}`);
+    // console.log(`--> displayProduct(product) => ${product.name}`);
     // console.log('product:', product);
 
     // image is not always provided
@@ -165,8 +133,8 @@ function displayProductCard(product) {
 }
 
 function displayProduct(product, prodId = 0) {
-     console.log(`--> displayProduct(product, ${prodId})`);
-     console.log('product:', product);
+    // console.log(`--> displayProduct(product, ${prodId})`);
+    // console.log('product:', product);
     
     if (prodId >= 1) {  // Show selected product
         const imagePath = (product.image_url) ? product.image_url : '';
@@ -234,14 +202,123 @@ function displayProduct(product, prodId = 0) {
     }
 }
 
-function displaySearch(arr = undefined) {
-    console.log('--> displaySearch()');
-    console.log('arr:', arr);
+/* ###### Search ###### */
+function clickPreviousPage() {
+    // console.log(`--> click clickPreviousPage() actPage: ${actPage}, searchString: '${searchString}'`);
+    if (searchString && !arrowPrevious.classList.contains('inactive') ) {
+        // console.log(`=> importSearch(${actPage - 1})`);
+        importSearch(actPage - 1);
+    }
+    else {
+        console.error('Clicked inactive .pager.left!?');
+    }
+}
 
+function clickNextPage() {  
+    // console.log(`--> click nextPage() actPage: ${actPage}, searchString: '${searchString}'`);
+    if (searchString && !arrowNext.classList.contains('inactive') ) {
+        // console.log(`=> importSearch(${actPage - 1})`);
+        importSearch(actPage + 1);
+    }
+    else {
+        console.error('Clicked inactive .pager.right!?');
+    }
+}
+
+function updateSearchNav(page = 0, more = false) {  // actPage, more
+    // console.log(`--> updateSearchNav(${page}, ${more})`);
+    
+    const searchNavigaton = document.querySelector('.search-navigation');
+    
+    if (page > 0) {
+        searchNavigaton.classList.add('active');
+        if (page > 1) {
+            searchNavigaton.children[0].classList.remove('inactive');
+        }
+        searchNavigaton.children[1].innerHTML = page;
+        if (more) {
+            searchNavigaton.children[2].classList.remove('inactive');
+        }   
+    }
+    else {
+        searchResults.innerHTML = '';  // Empty results
+        searchNavigaton.classList.remove('active');  // Inactivate search navigation
+        searchNavigaton.children[0].classList.add('inactive');
+        searchNavigaton.children[1].innerHTML = 0;
+        searchNavigaton.children[2].classList.add('inactive');
+    }
+}
+
+async function importSearch(pageNbr = 0) {  // value only from arrows!
+    // console.log(`--> importSearch(${pageNbr}) actPage: ` + pageNbr);
+
+    updateSearchNav();  // Empty previous result
+
+    const input = form.querySelector('input');
+
+    // loader?
+    
+    let url;  // = `https://api.punkapi.com/v2/beers?page=${pageNbr}&per_page=${MAX_SEARCH}&beer_name=${searchString}`;
+    let hasMore = false;
+    
+    if (pageNbr > 0) {  // Previous/next page
+        input.value = searchString;  // Set input to remove changes
+    }
+    else {  // New search
+        searchString = input.value.trim();
+        pageNbr = 1;
+    }
+    //  console.log('searchString: ' + searchString);
+
+    try {
+        url = `https://api.punkapi.com/v2/beers?page=${pageNbr}&per_page=${MAX_SEARCH}&beer_name=${searchString}`;
+        // console.log('url: ' + url);
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log('data: ', data);
+        
+        for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            const element = `<div class="link" data-id="${item.id}">${item.name}</div>`;
+            searchResults.insertAdjacentHTML('beforeend', element);
+        }
+
+        // Check if there are more search results...
+        try {
+            url = `https://api.punkapi.com/v2/beers?page=${pageNbr + 1}&per_page=${MAX_SEARCH}&beer_name=${searchString}`;
+            // console.log('url: ' + url);
+            const response = await fetch(url);
+            const data = await response.json();
+            // console.log('data 2: ', data);
+            
+            hasMore = (data.length > 0) ? true : false;
+        } catch (error) {
+            console.error('Error 2: ', error);
+        }
+
+        // Update page navigation
+        updateSearchNav(pageNbr, hasMore);
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+
+    if (pageNbr > 0) {
+        actPage = (pageNbr > 0) ? pageNbr : 1;
+    }
+    actPage = pageNbr;
+    // console.log('actPage: ' + actPage);
+
+}
+function displaySearch(arr = undefined) {
+    // console.log('--> displaySearch()');
+    // console.log('arr:', arr);
 
     closeOthers('search');
+
     scrollToTop();
 }
+
+
 
 
 
@@ -257,20 +334,16 @@ function scrollToTop() {
 
 function closeOthers(id) {
     // console.log(`--> closeOthers(${id})`);
-
-    // console.log('display:', display);
-
     for (let i = 0; i < display.children.length; i++) {
         const item = display.children[i];
-        // console.log(item.id);
 
         if (item.id === id) {
             item.classList.add('show');
-             console.log('show: ' + item.id);
+            // console.log('show: ' + item.id);
         }
         else if (item.id != '') {
             item.classList.remove('show');
-             console.log('hide: ' + item.id);
+            // console.log('hide: ' + item.id);
         }
     }    
 }
